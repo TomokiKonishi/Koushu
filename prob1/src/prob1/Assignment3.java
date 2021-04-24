b86fe43a073db4800a41e409fe1455b7adc41c9e
TomokiKonishi <77879745+TomokiKonishi@users.noreply.github.com>
2021-04-24 22:09:00 +0900

    課題としてコミット

--- prob1/src/prob1/Assignment3.java
+++ prob1/src/prob1/Assignment3.java
@@ -1,5 +1,25 @@
 package prob1;
+import java.util.Scanner;
 
 public class Assignment3 {
+	 public static void main(String args[]) {
+		    
+	      Scanner scanner = new Scanner(System.in);
+	 
+	      int nenrei = scanner.nextInt();
+	      int kakaku  = 0;
+	      
+	      if(nenrei >=16 ) {
+	    	  kakaku=500;
+	      } else if (nenrei <=12) {
+	    	  kakaku = 0;
+	      } else if (nenrei <=15) {
+	    	  kakaku = 300;
+	      }
+	  System.out.print("kakaku = ");
+     System.out.print(kakaku);
+	      scanner.close();
+	    }
+	  }
 
-}
+

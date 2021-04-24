0267edbe1e37111f9289c7bb9862fdac3f89c2c7
TomokiKonishi <77879745+TomokiKonishi@users.noreply.github.com>
2021-04-25 00:25:33 +0900

    課題の完了としてコミット

--- prob1/src/prob1/Assignment4.java
+++ prob1/src/prob1/Assignment4.java
@@ -1,5 +1,33 @@
 package prob1;
+import java.util.Scanner;
 
 public class Assignment4 {
+	 public static void main(String args[]) {
+		    
+	      Scanner scanner = new Scanner(System.in);
+	      int nenrei = scanner.nextInt();
+	      int umare = scanner.nextInt();
+	      int kakaku = 0;
+	      int mode = scanner.nextInt();//モード1ならand,0ならorで判定する
+	      
+	      if(mode==0) {
+	    	 if( (umare==2001)	&&(nenrei==10)) {
+	    		 kakaku=100;
+	    	 }
+	    		  else { kakaku=500;
+	    		 }
+	    		  
+	    	 }else if(mode==1) {
+	    		 if( (umare==2001)	||(nenrei==10)) {
+		    	 kakaku=100;
+	    		 }else {
+		    	 kakaku=500;
+		    		 }
+	    	 }
+	      scanner.close();
+	 	 System.out.print(kakaku);  }
+}  
+	 
 
-}
+
+	 
--- prob1/src/prob1/Assignment5.java
+++ prob1/src/prob1/Assignment5.java
@@ -1,5 +1,20 @@
 package prob1;
-
+import java.util.Scanner;
 public class Assignment5 {
-
+	 public static void main(String args[]) {
+		    
+	      Scanner scanner = new Scanner(System.in);
+	      int a = scanner.nextInt();
+	      int b = 5;
+	      int count = 0;
+	      
+	      while(a<b) {
+	    	  a=a+1;
+	    	  count=count+1;
+	    	  	  
+	      }
+	      System.out.println("aの値は"+ a +"です。");
+	      System.out.println("繰り返した回数は"+ count +"です。");
+	      scanner.close();
+	    }
 }

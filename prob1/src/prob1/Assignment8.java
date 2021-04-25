f1e35a259ddd54ecf764c62520918231318a3a32
TomokiKonishi <77879745+TomokiKonishi@users.noreply.github.com>
2021-04-25 21:17:16 +0900

    課題の完了としてコミット

--- prob1/src/prob1/Assignment8.java
+++ prob1/src/prob1/Assignment8.java
@@ -2,4 +2,12 @@ package prob1;
 
 public class Assignment8 {
 
+	
+	 public static void main(String args[]) {
+		 int a=0;
+		 for(int i=0;i<3;i++ ) {
+			 System.out.println("aの値は"+a+"です。");
+			 a=a+1;
+		 }
+	 }
 }

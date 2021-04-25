9207596089a2faa97336dedc87d80f64f235abc1
TomokiKonishi <77879745+TomokiKonishi@users.noreply.github.com>
2021-04-25 18:43:37 +0900

    課題としてコミット

--- prob1/src/prob1/Assignment6.java
+++ prob1/src/prob1/Assignment6.java
@@ -1,5 +1,15 @@
 package prob1;
 
 public class Assignment6 {
-
+	  public static void main(String args[]) {
+		  double yokin = 100000;//小数点以下を扱うのでdouble型を採用
+		  int nensu =1;
+		  while(nensu<4) {
+			  yokin=yokin*1.1;
+			  nensu=nensu+1;
+			  
+		  }
+		  
+		  System.out.print(yokin);
+	  }
 }
--- prob1/src/prob1/Assignment7.java
+++ prob1/src/prob1/Assignment7.java
@@ -1,5 +1,24 @@
 package prob1;
 
 public class Assignment7 {
-
+	  public static void main(String args[]) {
+		  int a=0;
+		  int b=0;
+		  int i=0;
+		  int h=3;//hの値を繰り返したい回数に設定すればよい
+		  
+		  while(i<h) {
+			 if(a>b) {
+				 b=b+1;
+				 System.out.println("a="+a+"b="+b+"i="+i);
+			 }else {
+				 a=a+1;
+				 System.out.println("a="+a+"b="+b+"i="+i);
+			 }
+			 i=i+1;
+			 
+		  }
+		  
+	  }
+	
 }

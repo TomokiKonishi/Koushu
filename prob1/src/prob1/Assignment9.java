fa5e8eecbed5e16a5fd9f4a98446a767575f5a9b
TomokiKonishi <77879745+TomokiKonishi@users.noreply.github.com>
2021-04-25 23:41:35 +0900

    課題としてコミット

--- prob1/src/prob1/Assignment9.java
+++ prob1/src/prob1/Assignment9.java
@@ -1,5 +1,17 @@
 package prob1;
 
 public class Assignment9 {
-
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++) {
+			int a=1;
+			rep(a);
+		}
+	}
+	public static int rep(int k) {
+		 System.out.print(k);
+		 k=k*2;
+		 return k;
+		
+	}
+	
 }
